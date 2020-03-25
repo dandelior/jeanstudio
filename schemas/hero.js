45112ef220c82c329087a67f5adfa7aafb3f5255
dandelior <jberrioscifuentes@gmail.com>
2020-03-24 22:29:20 -0300

    hero images

---
+++ schemas/hero.js
@@ -0,0 +1,25 @@
+export default {
+    title: "Imágenes de Portada",
+    name: "heroImages",
+    type: "document",
+    // fieldsets: [
+    //     {}
+    // ],
+    fields: [
+        {
+            title: "Portada Móbil",
+            name: "mobileHero",
+            type: "image"
+        },
+        {
+            title: "Portada Tablet",
+            name: "tabletHero",
+            type: "image"
+        },
+        {
+            title: "Portada Escritorio",
+            name: "DesktopHero",
+            type: "image"
+        },
+    ]
+}
